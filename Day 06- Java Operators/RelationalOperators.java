aad5ec01880fb564dec63b1faa447e5e293adab7
Priyansh Sharma <digitalhub.priyansh@gmail.com>
2026-09-08 12:22:05 +0530

    Add files via upload

---
+++ Day 06- Java Operators/RelationalOperators.java
@@ -0,0 +1,24 @@
+public class RelationalOperators {public static void main(String[] args) {
+    
+
+//Relational Operators (==, !=, <, >, <=, >=), always works with boolean datatype.
+int a  = 5;
+int b = 10;
+
+boolean c = (a==b);
+System.out.println("boolean c --> " + c); // Result = false 
+boolean d = (a!=b);
+System.out.println("boolean d --> " + d); // result = true
+boolean e = (a<b);
+System.out.println("boolean e --> " + e); // result = true
+boolean f = (a>b);
+System.out.println("boolean f --> " + f); // result = false
+boolean g = (a<=b);
+System.out.println("boolean g --> " + g);// result = true
+boolean h = (a>=b);
+System.out.println("boolean h -->" + h); // reult = false
+
+
+}
+    
+}
