396ca7e3cd20d33404600e7dcf05999379d34e54
Priyansh Sharma <digitalhub.priyansh@gmail.com>
2026-09-26 19:21:01 +0530

    Add files via upload

---
+++ Day19-Objects deepdrive/CallByValue.java
@@ -0,0 +1,15 @@
+public class CallByValue {
+    public static void main(String[] args) {
+        // call by value:
+        int x = 4;
+        int y = 5;
+            System.out.println(x + ", " + y);
+        addTen(x, y);
+            System.out.println(x + ", " + y);
+    }   
+    static void addTen(int x, int y){
+        x = x + 10;
+        y = y + 10;
+
+    } 
+}
---
+++ Day19-Objects deepdrive/CallByValue2.java
@@ -0,0 +1,22 @@
+public class CallByValue2 {
+    public static void main(String[] args) {
+        Random r1 = new Random(4, 5);
+            System.out.println(r1.x + ", " + r1.y);
+            addTen(r1);
+            System.out.println(r1.x + ", " + r1.y);
+        
+    }
+    static void addTen(Random r){
+r.x = r.x + 10;
+r.y = r.y + 10;
+    }
+}
+class Random{
+    int x;
+    int y;
+        Random(int x, int y){
+            this.x = x;
+           this.y = y;
+        }
+
+}
